c34c78c771c81f972f1afc92edd1efde022496e7
tuuznik <39033831+tuuznik@users.noreply.github.com>
2020-01-04 20:41:56 +0100

    Update population.py

--- project/src/population.py
+++ project/src/population.py
@@ -47,26 +47,30 @@ class Population:
                      for i in range(len(self.main_list)//2)] # otrzymamy listę par rozwiązań
         self.temporary_list = []
         for i in range(len(pair_list)):
-            crossing_point = randint(1, len(self.main_list[0])-1) #wylosowanie punktu krzyżowania
-            offspring_1 = pair_list[i][0][:crossing_point] + pair_list[i][1][crossing_point:]  # stworzenie pierwszego potomka
-            offspring_2 = pair_list[i][1][:crossing_point] + pair_list[i][0][crossing_point:]
+            crossing_point = randint(1, len(self.parameters.skills_matrix[0])-1) #wylosowanie punktu krzyżowania
+            offspring_1 = pair_list[i][0].company_employee_list[:crossing_point] + pair_list[i][1].company_employee_list[crossing_point:]  # stworzenie pierwszego potomka
+            offspring_2 = pair_list[i][1].company_employee_list[:crossing_point] + pair_list[i][0].company_employee_list[crossing_point:]
             self.temporary_list.append(offspring_1)
             self.temporary_list.append(offspring_2)
 
-    def mutation(self, parameters: Parameters, mut_prob: float = 0.01):
+    def mutation(self, mut_prob: float = 0.1):
+        "Mutacja z prawdopodbienstwem mut_prob"
         for i in range(len(self.temporary_list)):
             for j in range(len(self.temporary_list[i])):
                 temp_gene = self.temporary_list[i][j]
                 prob = random()
                 if prob <= mut_prob:        #dla każdego genu losujemy liczbę z przedziału (0,1) i sprawdzamy, czy jest mniejsza od prawd. mutacji
                      while temp_gene == self.temporary_list[i][j]:
-                        temp_gene = randint(0, len(parameters.skills_matrix) - 1)
+                        temp_gene = randint(0, len(self.parameters.skills_matrix) - 1)
                 self.temporary_list[i][j] = temp_gene
 
-    def merging(self):
+    def merging(self) -> List:
+        "Laczenie populacji rodzicielskiej z potomkami, sortowanie wg f.adaptacji oraz wyznaczenie nowej populacji z najlepszych osobnikow"
+        self.temporary_list = [Solution(len(self.parameters.skills_matrix), len(self.parameters.skills_matrix[0]), self.temporary_list[i]) for i in range(len(self.temporary_list))]
         merged_list = self.main_list + self.temporary_list      #złączenie list rodziców i potomków
         merged_list.sort(key=lambda obj: obj.adaptation(self.parameters), reverse=True)     #posortowanie wszystkich osobników od najlepszego do najgorszego
         merged_list = merged_list[:len(self.main_list)]
         self.main_list = merged_list        #powstaje lista nowych rodziców 
-    # TODO: dodać operatory krzyżowania/mutacji/złączenia list, zrobić testy, zwłaszcza dla dotychczas napisanych
-    #  funkcjonalości Population, ew. pododawać błędy i ich obsługi
+    
+    # TODO zrobić testy, zwłaszcza dla dotychczas napisanych
+    #  pododawać błędy i ich obsługi
